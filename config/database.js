5d1f105384fe9c50c38c3991e19cbed33ec63ca3
khaireddine <ihrissanek@gmail.com>
2024-08-27 00:38:12 +0100

    updated-commit

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql'
 });
